246795dd86bd1db9f6ddbba3b3292781e2debedc
Benyi Gong <benyigong1995@gmail.com>
2020-05-27 15:57:58 -0700

    db

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'miracle',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
